f0466403b51f98c81742e80f0c7036e9b8430777
Ricky Zhu <riozhu@gmail.com>
2016-09-15 20:39:06 -0700

    first

---
+++ test01/app01.rb
@@ -0,0 +1,6 @@
+# Write a method that will take an will return a hash whose keys are the  array elements and the values will be either the number to the power two if it's a number or the length of the string if it's a string. For example if the given array is:
+# ["Hello CodeCore", 5, "Hi"]
+# the method will return:
+# {"Hello CodeCore" => 14, 5 => 25, "Hi" => 2}
+
+def change
